f28bc83cc050a0ac6fe888324d017bbf060af61c
Calvin <creativomiel@gmail.com>
2025-02-01 01:35:13 -0500

    update frontend

--- backend/inventarios/src/main/java/calvin/inventarios/repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package calvin.inventarios.repository;
-
-import calvin.inventarios.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductRepository extends JpaRepository<Product, Long>
-{
-
-}
